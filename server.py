9f2cb9efbf951bcde7da9074c2696f777edc42e4
xiurobert <xiurobert@gmail.com>
2018-10-20 16:21:45 +0800

    jia cheng is doubly gay

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
-#1/bin/env/python
+#!/bin/env/python
 
 
 from app import *
 
 if __name__ == "__main__":
     print("Running Flask Development Server on port 5000")
+    rafael.jinja_env.auto_reload = True
+    rafael.config["TEMPLATES_AUTO_RELOAD"] = True
     rafael.run(use_reloader=True)
