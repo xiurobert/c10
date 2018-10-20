9672171f69d8fcaf559aefdd432ff7fe6825e2c1
xiurobert <xiurobert@gmail.com>
2018-10-20 13:58:25 +0800

    jia cheng is gay

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 #!/bin/env/python
 
+from flask import Flask, render_template
+import json
+import requests
+
+rafael = Flask(__name__)
+
+
+@rafael.route("/")
+def index():
+
+    returnedjson = requests.get("https://launchlibrary.net/1.4/launch/next/5").json()
+    print(returnedjson)
+    return render_template("index.html", launches=returnedjson["launches"])
---
+++ server.py
@@ -0,0 +1,8 @@
+#1/bin/env/python
+
+
+from app import *
+
+if __name__ == "__main__":
+    print("Running Flask Development Server on port 5000")
+    rafael.run(use_reloader=True)
